dcafdde05565d88c4cdb3b4ecaafad52028a02dd
yoonhoShin <amuse1991@gmail.com>
2019-04-10 22:31:42 +0900

    contact 작성
    contact form을 작성

--- client/src/components/body/contact/Contact.js
+++ client/src/components/body/contact/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Element} from 'react-scroll';
+import { Container, Row, Col,Button, Form, FormGroup, Label, Input, FormText} from "reactstrap";
 
 export default class Contact extends Component{
     
@@ -10,10 +11,38 @@ export default class Contact extends Component{
 
     render(){
         return(
-            <Element name="contact" className="element">
-                <div style={this.divStyle}>
-                    contact
-                </div>
+            <Element name="contact" className="element" style={this.divStyle}>
+                <Container fluid>
+                    <Row>
+                        <Col className="page-header">
+                            <h1>CONTACT</h1>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <h6>궁금한 점이 있으신가요?</h6>
+                            <Form>
+                                <FormGroup>
+                                    {/* <Label for="senderEmail">Email</Label> */}
+                                    <Input type="email" name="email" id="senderEmail" placeholder="Enter email" />
+                                </FormGroup>
+                                <FormGroup>
+                                    {/* <Label for="title">Title</Label> */}
+                                    <Input type="text" name="title" id="title" placeholder="Tltle" />
+                                </FormGroup>
+                                <FormGroup>
+                                    {/* <Label for="message">Message</Label> */}
+                                    <Input type="textarea" name="message" id="message" placeholder="Message" />
+                                </FormGroup>
+                                <FormGroup>
+                                    {/* <Label for="exampleFile">File</Label> */}
+                                    <Input type="file" name="file" id="exampleFile" />
+                                </FormGroup>
+                                <Button>Submit</Button>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
             </Element>
         )
     }
