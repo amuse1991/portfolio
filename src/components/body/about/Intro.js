3e9c49797b9c1e910f040e770ef62478c8adc4e7
amuse1991 <amuse1991@gmail.com>
2020-08-13 19:02:30 +0900

    텍스트 일부 수정

--- src/components/body/about/Intro.js
+++ src/components/body/about/Intro.js
@@ -1,59 +1,70 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import Keywords from "./Keywords";
-import { Button, UncontrolledTooltip } from 'reactstrap';
+import { Button, UncontrolledTooltip } from "reactstrap";
 import "../../../static/css/intro.css";
 
-export default class Intro extends Component{
+export default class Intro extends Component {
+  render() {
+    return (
+      <React.Fragment className="intro-wrapper">
+        <p className="text-center intro-text">
+          안녕하세요. <br />
+          프론트엔드 개발자 신윤호입니다.
+          <br />
+          웹 개발에 흥미를 가지고 컴퓨터공학과에 편입하여 본격적으로 개발을
+          시작했으며
+          <br />
+          재학 기간 동안
+          <span id="popover-related-class" className="popover-span">
+            관련 수업 이수
+          </span>
+          ,
+          <span id="popover-internship" className="popover-span">
+            웹 개발 직무 인턴쉽
+          </span>
+          을 수행했습니다.
+          <br />
+          뿐만 아니라 웹을 주제로
+          <span id="popover-graduation-project" className="popover-span">
+            졸업 프로젝트
+          </span>
+          를 진행했으며,
+          <span id="popover-personal-project" className="popover-span">
+            개인 프로젝트
+          </span>
+          또한 진행했습니다.
+          <br />
+          저는 개발자로써 다음과 같은 가치관과 역량을 지니고 있습니다. <br />
+          <br />
+        </p>
+        <Keywords title="Can-Do Attitude" />
+        <Keywords title="Responsible" />
+        <Keywords title="Self-Motivated" />
+        <Keywords title="Cooperative" />
 
-    render(){
-        return(
-            <React.Fragment className="intro-wrapper">
-                <p className="text-center intro-text">
-                    안녕하세요. <br/>
-                    신입 백엔드 개발자 신윤호입니다.<br/>
-                    백엔드 개발에 흥미를 가지고 컴퓨터공학과에 편입하여 본격적으로 개발을 시작했으며<br/>
-                    재학 기간 동안 
-                    <span id="popover-related-class" className="popover-span">관련 수업 이수</span>, 
-                    <span id="popover-internship" className="popover-span">웹 개발 직무 인턴쉽</span>을 수행했습니다.<br/> 
-                    뿐만 아니라 웹을 주제로 
-                    <span id="popover-graduation-project" className="popover-span">졸업 프로젝트</span>
-                    를 진행했으며, 
-                    <span id="popover-personal-project" className="popover-span">개인 프로젝트</span>
-                    또한 진행했습니다.<br/>
-                    저는 개발자로써 다음과 같은 가치관과 역량을 지니고 있습니다. <br/>
-                    <br/>
-                </p>
-                <Keywords title="Can-Do Attitude"/>
-                <Keywords title="Responsible"/>
-                <Keywords title="Self-Motivated"/>
-                <Keywords title="Cooperative"/>
-
-                <UncontrolledTooltip placement="top" target="popover-related-class">
-                    <span>
-                        웹 프로그래밍 <br/>
-                        모바일 소프트웨어 설계 <br/>
-                        데이터베이스 <br/>
-                        컴퓨터 네트워크 <br/>
-                        분산처리 <br/>
-                        시스템 및 네트워크 보안 <br/>
-                    </span>
-                </UncontrolledTooltip>
-                <UncontrolledTooltip placement="top" target="popover-internship">
-                    <span>
-                        사내 업무 평가 관리 웹 어플리케이션 개발 
-                    </span>
-                </UncontrolledTooltip>
-                <UncontrolledTooltip placement="top" target="popover-graduation-project">
-                    <span>
-                        인공위성 관제 모니터링 웹 서비스 개발 
-                    </span>
-                </UncontrolledTooltip>
-                <UncontrolledTooltip placement="top" target="popover-personal-project">
-                    <span>
-                        Study Time 개발
-                    </span>
-                </UncontrolledTooltip>
-            </React.Fragment>
-        );
-    }
+        <UncontrolledTooltip placement="top" target="popover-related-class">
+          <span>
+            웹 프로그래밍 <br />
+            모바일 소프트웨어 설계 <br />
+            데이터베이스 <br />
+            컴퓨터 네트워크 <br />
+            분산처리 <br />
+            시스템 및 네트워크 보안 <br />
+          </span>
+        </UncontrolledTooltip>
+        <UncontrolledTooltip placement="top" target="popover-internship">
+          <span>사내 업무 평가 관리 웹 어플리케이션 개발</span>
+        </UncontrolledTooltip>
+        <UncontrolledTooltip
+          placement="top"
+          target="popover-graduation-project"
+        >
+          <span>인공위성 관제 모니터링 웹 서비스 개발</span>
+        </UncontrolledTooltip>
+        <UncontrolledTooltip placement="top" target="popover-personal-project">
+          <span>Study Time 개발</span>
+        </UncontrolledTooltip>
+      </React.Fragment>
+    );
+  }
 }
--- src/components/body/home/Home.js
+++ src/components/body/home/Home.js
@@ -1,31 +1,41 @@
-import React, { Component } from 'react';
-import {Element} from 'react-scroll';
-import { Button, Row, Col } from 'reactstrap';
-import {Link} from "react-scroll";
-import '../../../static/css/home.css';
+import React, { Component } from "react";
+import { Element } from "react-scroll";
+import { Button, Row, Col } from "reactstrap";
+import { Link } from "react-scroll";
+import "../../../static/css/home.css";
 
-
-export default class Home extends Component{
-    divStyle = {
-        width:'100%',
-        height:'100vh'
-    };
-    render(){
-        return(
-            <Element name="home" className="element">
-            <Row>
-                <Col>
-                <div className="home-jumbotron" style={this.divStyle}>
-                    <h1 className="title">Hello, I'm <strong>Yoonho Shin</strong>. <br/>
-                    I'm backend web developer.</h1>
-                    <Link className="nav-link" to="about" spy={true} smooth={true}
-                        offset={-50} duration={500} onSetActive={this.handleSetActive}>
-                        <Button color="primary" size="lg">View my work</Button>
-                    </Link>    
-                </div>
-                </Col>
-            </Row>
-            </Element>
-        )
-    }
+export default class Home extends Component {
+  divStyle = {
+    width: "100%",
+    height: "100vh"
+  };
+  render() {
+    return (
+      <Element name="home" className="element">
+        <Row>
+          <Col>
+            <div className="home-jumbotron" style={this.divStyle}>
+              <h1 className="title">
+                Hello, I'm <strong>Yoonho Shin</strong>. <br />
+                I'm frontend web developer.
+              </h1>
+              <Link
+                className="nav-link"
+                to="about"
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={500}
+                onSetActive={this.handleSetActive}
+              >
+                <Button color="primary" size="lg">
+                  View my works
+                </Button>
+              </Link>
+            </div>
+          </Col>
+        </Row>
+      </Element>
+    );
+  }
 }
